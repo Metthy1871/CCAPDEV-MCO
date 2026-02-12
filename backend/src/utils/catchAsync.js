6ebf8c299b8643d46e89a1ffe9d5fa765bb9cf78
yue-matcha <thea_delacruz@dlsu.edu.ph>
2026-02-12 23:54:23 +0800

    feat(utils): add catchAsync utility for async error handling

---
+++ backend/src/utils/catchAsync.js
@@ -0,0 +1,10 @@
+// Source: https://medium.com/@santoshgiri2345/simplifying-error-handling-in-express-js-with-catchasync-8a0a561ada8f
+// to beused for wrapping async functions to reduce manual try-catch blocks
+
+const catchAsync = (fn) => {
+    return (req, res, next) => {
+        fn(req, res, next).catch((err) => next(err));
+    };
+};
+
+export default catchAsync;
