9ef98d999ca20ce4c87ac16fb1705af9626c3f80
yue-matcha <thea_delacruz@dlsu.edu.ph>
2026-03-05 12:09:57 +0800

    feat(utils): add constants for comment sorting

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -2,5 +2,12 @@
 export const SORT_POSTS_OPTIONS = {
     RECENT: 'recent',
     POPULAR_ALL_TIME: 'popular_all_time',
-    POPULAR_RRECENT: 'popularRecent'
+    POPULAR_RECENT: 'popularRecent'
 };
+
+export const SORT_COMMENTS_OPTIONS = {
+    OLDEST: 'oldest',
+    POPULAR: 'popular'
+};
+
+
